7ff7f5640b1df1b5b933f437be8409ce8d51ac2c
Victor Alexa <victor.d.alexa@gmail.com>
2026-08-18 22:55:02 +0300

    feat: change Footer wrapper from div to footer for semantic HTML

--- frontend/components/footer.tsx
+++ frontend/components/footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className='text-sm text-muted-foreground pt-2 pb-5 flex flex-col sm:flex-row items-center justify-center gap-y-2 sm:gap-x-20 bg-transparent border-t border-gray-200'>
+    <footer className='text-sm text-muted-foreground pt-2 pb-5 flex flex-col sm:flex-row items-center justify-center gap-y-2 sm:gap-x-20 bg-transparent border-t border-gray-200'>
       <a
         href='https://github.com/itsthevictor/risk'
         className='hover:underline hover:text-foreground'
@@ -19,7 +19,7 @@ const Footer = () => {
       >
         CV
       </a>
-    </div>
+    </footer>
   );
 };
 export default Footer;
