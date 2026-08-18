bb33af25776925a4e63eefec4d63c720a07cda87
Victor Alexa <victor.d.alexa@gmail.com>
2026-08-18 22:54:38 +0300

    feat: update Footer layout to improve responsiveness and alignment

--- frontend/components/footer.tsx
+++ frontend/components/footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className='text-sm text-muted-foreground pt-2 pb-5 flex flex-row items-start justify-center gap-x-20 bg-transparent border-t border-gray-200'>
+    <div className='text-sm text-muted-foreground pt-2 pb-5 flex flex-col sm:flex-row items-center justify-center gap-y-2 sm:gap-x-20 bg-transparent border-t border-gray-200'>
       <a
         href='https://github.com/itsthevictor/risk'
         className='hover:underline hover:text-foreground'
