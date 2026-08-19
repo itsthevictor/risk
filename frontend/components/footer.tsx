579fa7829caa0334d21a2dc067d71e33758542b4
Victor Alexa <victor.d.alexa@gmail.com>
2026-08-19 08:32:10 +0300

    feat: update border color in Footer for improved styling

--- frontend/components/footer.tsx
+++ frontend/components/footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className='text-sm text-muted-foreground py-3 md:flex flex-row items-center justify-end gap-y-2 sm:gap-x-20 bg-transparent border-t border-gray-200 hidden pr-4'>
+    <footer className='text-sm text-muted-foreground py-3 md:flex flex-row items-center justify-end gap-y-2 sm:gap-x-20 bg-transparent border-t border-muted-foreground hidden pr-4'>
       <a
         href='https://github.com/itsthevictor/risk'
         className='hover:underline hover:text-foreground'
