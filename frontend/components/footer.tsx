1e2a754d02b5d7a5529a367bd83c7a82c5a1baf4
Victor Alexa <victor.d.alexa@gmail.com>
2026-08-19 08:20:30 +0300

    feat: update Footer layout and add target attributes for improved accessibility

--- frontend/components/footer.tsx
+++ frontend/components/footer.tsx
@@ -1,21 +1,27 @@
 const Footer = () => {
   return (
-    <footer className='text-sm text-muted-foreground pt-2 pb-5 md:flex flex-row items-center justify-center gap-y-2 sm:gap-x-20 bg-transparent border-t border-gray-200 hidden'>
+    <footer className='text-sm text-muted-foreground py-3 md:flex flex-row items-center justify-end gap-y-2 sm:gap-x-20 bg-transparent border-t border-gray-200 hidden'>
       <a
         href='https://github.com/itsthevictor/risk'
         className='hover:underline hover:text-foreground'
+        target='_blank'
+        rel='noopener noreferrer'
       >
         GitHub
       </a>
       <a
         href='https://www.linkedin.com/in/victor-alexa/'
         className='hover:underline hover:text-foreground'
+        target='_blank'
+        rel='noopener noreferrer'
       >
         LinkedIn
       </a>
       <a
         href='/path/to/cv.pdf'
         className='hover:underline hover:text-foreground'
+        target='_blank'
+        rel='noopener noreferrer'
       >
         CV
       </a>
