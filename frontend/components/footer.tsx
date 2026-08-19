624330c51f43bf022bdfa2e63460467ebc316a27
Victor Alexa <victor.d.alexa@gmail.com>
2026-08-19 14:38:58 +0300

    feat: update text content in Home, Header, and Footer for clarity and consistency

--- frontend/components/footer.tsx
+++ frontend/components/footer.tsx
@@ -1,9 +1,15 @@
 import { ThemeToggle } from '@/components/theme-toggle';
+// import { Separator } from '@/components/ui/separator';
+// import Link from 'next/link';
 const Footer = () => {
   return (
     <footer className='text-xs text-muted-foreground py-3 md:flex flex-row items-center justify-between gap-y-2  bg-transparent border-t border-muted-foreground hidden px-4'>
       <ThemeToggle />
       <div className='flex flex-row gap-x-10'>
+        {/* <Link href='/despre' className='hover:underline hover:text-foreground'>
+          Despre
+        </Link>
+        <Separator orientation='vertical' className='h-4' /> */}
         <a
           href='https://github.com/itsthevictor/risk'
           className='hover:underline hover:text-foreground'
